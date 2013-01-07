e0982e63321dfdf88cc63afa35d699af302f5a56
justinwup <justin@winduppixel.com>
2013-01-06 20:39:45 -0600

    works

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-	def getTime(stop)
-		doc = Nokogiri::HTML(open("http://www.ctabustracker.com/bustime/api/v1/getpredictions?key=kRBbPLQJ5L8GXuCeTdRpKU8Xg&rt=9&stpid=#{stop}"))
+	def getTime(route,stop)
+		doc = Nokogiri::HTML(open("http://www.ctabustracker.com/bustime/api/v1/getpredictions?key=kRBbPLQJ5L8GXuCeTdRpKU8Xg&rt=#{route}&stpid=#{stop}"))
 		@doc = Hash.from_xml(doc.to_s)
 		render 'bustimes/time'
 	end
